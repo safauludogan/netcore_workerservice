5f6dd56a305206e20ae77003b01a0fea631a053d
safauludogan <suludogan95@gmail.com>
2024-01-03 12:15:53 +0300

    Request with Idenfit login

--- DataAccess/DBModel/DataContext.cs
+++ DataAccess/DBModel/DataContext.cs
@@ -12,14 +12,6 @@ public partial class DataContext : DbContext
 		: base(options)
 	{
 	}
-
-
-
-	/*protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-		=> optionsBuilder.UseSqlServer("Server=DESKTOP-D4CPGQD\\SQLSERDEV;Database=VIBE_DEKA;User Id=sa;password=sas;Trusted_Connection=True;TrustServerCertificate=true;");*/
-
-
 	public virtual DbSet<MikroIdenfit> MikroIdenfits { get; set; }
 
 	public virtual DbSet<PersonelTransaction> PersonelTransactions { get; set; }
--- WindowsServiceDEKA/Worker.cs
+++ WindowsServiceDEKA/Worker.cs
@@ -1,5 +1,11 @@
 using DataAccess;
 using DataAccess.DBModel;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Logging;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
 
 namespace WindowsServiceDEKA
 {
@@ -23,8 +29,10 @@ namespace WindowsServiceDEKA
 
 				}
 				//PrintUserInfo(dbRepository.GetAllPersonel());
-				PrintMicroData(dbRepository.GetMicroDatas());
+				//PrintMicroData(dbRepository.GetMicroDatas());
+				await Login();
 				await Task.Delay(3000, stoppingToken);
+
 			}
 		}
 
@@ -43,6 +51,59 @@ namespace WindowsServiceDEKA
 			foreach (var data in datas)
 			{
 				_logger.LogInformation($"Sicil No: {data.StaffNumber} --- AdSoyad: {data.FirstName} {data.LastName} --- Þirket: {data.Company}");
+			}
+		}
+
+		private async Task Login()
+		{
+			using (HttpClient httpClient = new HttpClient())
+			{
+				string apiUrl = "https://uatapi.idenfit.com/login";
+
+				// Burada gerektiðiniz headers, authentication vb. ayarlamalarý yapabilirsiniz.
+				// httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer token");
+				//httpClient.DefaultRequestHeaders.Add("From", "mobile");
+				//httpClient.DefaultRequestHeaders.Add("Content-Type", "application/x-www-form-urlencoded");
+				
+				httpClient.DefaultRequestHeaders.From = "mobile";
+				httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/x-www-form-urlencoded"));
+
+				var query = new Dictionary<string, string>
+				{
+					["username"] = "alper.tomacoglu@vibebilisim.com",
+					["password"] = "123456",
+				};
+
+				// Veriyi form-urlencoded formatýna dönüþtürme
+				var content = new FormUrlEncodedContent(query);
+
+				try
+				{
+					HttpResponseMessage response = await httpClient.PostAsync(apiUrl, content);
+
+					if (response.IsSuccessStatusCode)
+					{
+						string responseContent = await response.Content .ReadAsStringAsync();
+						Console.WriteLine("Diðer API'den gelen veri: " + responseContent);
+						Console.WriteLine("*******************************");
+
+						// Bearer token'ý headers içerisinden alma
+						if (response.Headers.TryGetValues("Authorization", out var tokenValues))
+						{
+							string bearerToken = tokenValues.First();
+							Console.WriteLine("Bearer Token: " + bearerToken);
+						}
+
+					}
+					else
+					{
+						Console.WriteLine("Hata kodu: " + response.StatusCode);
+					}
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine("Hata: " + ex.Message);
+				}
 			}
 		}
 	}
